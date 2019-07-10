bf0a2fdad1f7b6b1468a27475bb47f47efecb5ee
mouad54 <47031199+mouad54@users.noreply.github.com>
2019-07-10 16:40:28 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By ! DL |şKƴXş_ɱą");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("537286861903888396")
+setInterval(function() {
+channel.send(`spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam`);
+}, 30)
+})
+
+client.login(process.env."NDA1MDAwNTMxODk2MDQxNDgz.XOsluQ.piwHvzlg57y5jfNH0I373XHxY3c");
